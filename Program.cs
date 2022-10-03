8533e70eb274b6149b64c4a07fcd7437c8e86ce5
Mariamagdalina <metodist_rmmc@mail.ru>
2022-10-03 21:45:06 +0300

    этап 2. добавлен подсчет символов в строках и формирование нового массива

--- Program.cs
+++ Program.cs
@@ -2,16 +2,35 @@
 Console.Write("Введите количество строк: ");
 int size = Convert.ToInt32(Console.ReadLine());
 // объявляем массив
-string[] arrString; 
+string[] arrString, arrNewString; 
 arrString = new string[size];
+arrNewString = new string[size];
+
 // заполняем массив элементами
 for(int i=0; i < size ; i++)
 {
    Console.Write("Введите новую строку ");
    arrString[i] = Console.ReadLine();
 }
-// выводим массив на экран
+
+// подсчитываем количество символов в строках
+ int j = 0; // индекс для нового массива
 for(int i=0; i < size ; i++)
 {
-   Console.WriteLine(arrString[i]);
+   int quantity = 0; // переменная для подсчета символов в строке
+  
+   foreach(var ch in arrString[i] )
+   {
+      quantity++;
+   }
+   if(quantity <= 3) // если количество символов <=3 записываем элемент в новый массив
+     {
+       arrNewString[j] = arrString[i];
+       j++;
+     } 
+}
+// выводим новый массив
+for(int i=0; i < j ; i++)
+{
+   Console.WriteLine(arrNewString[i]);
 }
